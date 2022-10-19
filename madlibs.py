eb14551f939d2449c3128d1241307072e2159cfb
Simran <115931644+simranwork@users.noreply.github.com>
2022-10-20 02:51:30 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+
+Name =input("Name:")
+game = input("game:")
+age = input("age:")
+city = input("city:")
+
+madlib = (f"Hello! My name is {Name} and I love to paly {game}. I am {age} years old and I live in {city}")
+print(madlib)
